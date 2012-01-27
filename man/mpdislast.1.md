22e6c2bc05f208bd3c06fd00e775d445e486a797
Alessandro Ghedini <al3xbio@gmail.com>
2012-01-27 23:08:23 +0100

    add mpdislast utility

---
+++ man/mpdislast.1.md
@@ -0,0 +1,33 @@
+mpdislast(1) -- stop playback after the currently playing song
+==============================================================
+
+## SYNOPSIS
+
+`mpdislast [OPTIONS]`
+
+## DESCRIPTION
+
+**mpdislast** is a simple utility that stops the MPD playback after the song
+that is currently playing ends.
+
+## OPTIONS ##
+
+`-a, --addr`
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+The MPD server address.
+
+`-p, --port`
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+The MPD server port.
+
+## AUTHOR ##
+
+Alessandro Ghedini <al3xbio@gmail.com>
+
+## COPYRIGHT ##
+
+Copyright (C) 2012 Alessandro Ghedini <al3xbio@gmail.com>
+
+This program is released under the 3 clause BSD license.
---
+++ src/mpdislast.c
@@ -0,0 +1,135 @@
+/*
+ * Collection of more or less useful MPD tools.
+ *
+ * Copyright (c) 2012, Alessandro Ghedini
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of the mpdutils project, Alessandro Ghedini, nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <mpd/client.h>
+
+static struct option long_opts[] = {
+	{ "addr",	required_argument,	0, 'a' },
+	{ "port",	required_argument,	0, 'p' },
+	{ "help",	no_argument,		0, 'h' },
+	{ 0,		0,			0,  0  }
+};
+
+static inline void help() {
+	#define CMD_HELP(CMDL, CMDS, MSG) printf("  %s, %s\t%s.\n", CMDS, CMDL, MSG);
+
+	puts("Usage: mpdislast [OPTIONS]\n");
+	puts(" Options:");
+
+	CMD_HELP("--addr",	"-a",	"The MPD server address");
+	CMD_HELP("--port",	"-p",	"The MPD server port");
+	CMD_HELP("--help",	"-h",	"Show this help");
+
+	puts("");
+}
+
+int main(int argc, char *argv[]) {
+	int opts;
+
+	unsigned int current = 0;
+
+	struct mpd_song *song;
+	struct mpd_connection *mpd;
+
+	char *mpd_addr = NULL;
+	int   mpd_port = 0;
+
+	while ((opts = getopt_long(argc, argv, "a:p:h", long_opts, 0)) != -1) {
+		switch (opts) {
+			case 'a': { mpd_addr = optarg;		break;     }
+			case 'p': { mpd_port = atoi(optarg);	break;     }
+			default :
+			case 'h': { help();			return -1; }
+		}
+	}
+
+	mpd = mpd_connection_new(mpd_addr, mpd_port, 30000);
+
+	if (mpd_connection_get_error(mpd) != MPD_ERROR_SUCCESS) {
+		mpd_connection_free(mpd);
+		return -1;
+	}
+
+	song = mpd_run_current_song(mpd);
+
+	if (mpd_connection_get_error(mpd) != MPD_ERROR_SUCCESS) {
+		mpd_connection_free(mpd);
+		return -1;
+	}
+
+	current = mpd_song_get_id(song);
+
+	mpd_song_free(song);
+
+	while (true) {
+		unsigned int got = 0;
+
+		mpd_run_idle_mask(mpd, MPD_IDLE_PLAYER);
+
+		if (mpd_connection_get_error(mpd) != MPD_ERROR_SUCCESS) {
+			mpd_connection_free(mpd);
+			return -1;
+		}
+
+		song = mpd_run_current_song(mpd);
+
+		if (mpd_connection_get_error(mpd) != MPD_ERROR_SUCCESS) {
+			mpd_connection_free(mpd);
+			return -1;
+		}
+
+		got = mpd_song_get_id(song);
+		mpd_song_free(song);
+
+		if (current != got) {
+			mpd_run_stop(mpd);
+
+			if (mpd_connection_get_error(mpd) != MPD_ERROR_SUCCESS) {
+				mpd_connection_free(mpd);
+				return -1;
+			}
+
+			mpd_connection_free(mpd);
+			return 0;
+		}
+	}
+
+	mpd_connection_free(mpd);
+
+	return 0;
+}
